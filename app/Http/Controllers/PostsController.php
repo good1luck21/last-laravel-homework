dba6970f1e913cc1ccb3be87948fe542d973f472
good1luck21 <good1luck21@gmail.com>
2022-11-19 15:44:47 +0900

    first commit

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Post;
+
+class PostsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+      $posts = Post::all();
+      // return all posts to react as json
+      return response()->json($posts);
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+  // save post to database
+      $post = new Post;
+      $post->title = $request->title;
+      $post->body = $request->body;
+      $post->save();
+      // return a success message to react
+      return response()->json(['success' => 'Post saved successfully']);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+    // return a post to react
+      $post = Post::find($id);
+      return response()->json($post);
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
